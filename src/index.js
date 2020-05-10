57d973974e5faa17d7dfc3504ec8734a04e25f1e
dmc2015 <mclamb.donald@gmail.com>
2020-05-10 10:02:40 -0400

    Changes

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ import { InMemoryCache } from 'apollo-cache-inmemory';
 // }
 
 const link = createHttpLink({
-  uri: "http:/localhost:5000"
+  uri: "http:/localhost:5000/graphql"
 })
 
 const client = new ApolloClient({
